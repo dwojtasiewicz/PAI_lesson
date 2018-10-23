cbcf67805cbac972bb60834b43be34db71c7d2fb
Adrian Widłak <adrian.widlak@tvt.biz>
2018-10-23 09:01:36 +0200

    App structure

---
+++ controllers/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once("AppController.php");
+
+
+class DefaultController extends AppController
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function login()
+    {
+        //renderowanie szablonu z katalogu views
+    }
+}
