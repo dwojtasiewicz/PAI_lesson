8276ca1e04def0ca9debd172f79d912f22e19680
Adrian Widłak <adrian.widlak@tvt.biz>
2018-12-04 08:48:57 +0100

    Add bootsrap library and basic css

--- views/DefaultController/login.php
+++ views/DefaultController/login.php
@@ -5,19 +5,31 @@
 
 <body>
 
-<h1>LOGIN</h1>
+<div class="container">
+    <div clas="row">
+        <div class="col-sm-6 offset-sm-3">
+            <h1>LOGIN</h1>
 
-<?php if(isset($message)): ?>
-    <?php foreach($message as $item): ?>
-        <div><?= $item ?></div>
-    <?php endforeach; ?>
-<?php endif; ?>
+            <?php if(isset($message)): ?>
+                <?php foreach($message as $item): ?>
+                    <div><?= $item ?></div>
+                <?php endforeach; ?>
+            <?php endif; ?>
 
-<form action="?page=login" method="POST">
-    <input name="email" placeholder="email" required/>
-    <input name="password" placeholder="password" type="password" required/>
-    <input type="submit" value="Sign in"/>
-</form>
+            <form action="?page=login" method="POST">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" name="email" placeholder="email" required/>
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword">Password</label>
+                    <input type="password" name="password" class="form-control" id="inputPassword" placeholder="password" type="password" required/>
+                </div>
+                <input type="submit" value="Sign in" class="btn btn-primary btn-lg" />
+            </form>
+        </div>
+    </div>
+</div>
 
 </body>
 </html>
