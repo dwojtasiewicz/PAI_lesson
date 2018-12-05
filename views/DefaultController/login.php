bd6751d5ff104e6fdca5ae5b76b91b7ab72c7836
adrianwii <adrian.widlak@interia.pl>
2018-12-05 09:19:09 +0100

    Add material icons

--- views/DefaultController/login.php
+++ views/DefaultController/login.php
@@ -9,7 +9,7 @@
     <div clas="row">
         <div class="col-sm-6 offset-sm-3">
             <h1>LOGIN</h1>
-
+            <hr>
             <?php if(isset($message)): ?>
                 <?php foreach($message as $item): ?>
                     <div><?= $item ?></div>
@@ -17,15 +17,23 @@
             <?php endif; ?>
 
             <form action="?page=login" method="POST">
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" name="email" placeholder="email" required/>
+                <div class="form-group row">
+                    <label for="inputEmail" class="col-sm-1 col-form-label">
+                        <i class="material-icons md-48">email</i>
+                    </label>
+                    <div class="col-sm-11">
+                        <input type="email" class="form-control" id="inputEmail" name="email" placeholder="email" required/>
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label for="inputPassword">Password</label>
-                    <input type="password" name="password" class="form-control" id="inputPassword" placeholder="password" type="password" required/>
+                <div class="form-group row">
+                    <label for="inputPassword" class="col-sm-1 col-form-label">
+                        <i class="material-icons md-48">person</i>
+                    </label>
+                    <div class="col-sm-11">
+                        <input type="password" name="password" class="form-control" id="inputPassword" placeholder="password" type="password" required/>
+                    </div>
                 </div>
-                <input type="submit" value="Sign in" class="btn btn-primary btn-lg" />
+                <input type="submit" value="Sign in" class="btn btn-primary btn-lg float-right" />
             </form>
         </div>
     </div>
