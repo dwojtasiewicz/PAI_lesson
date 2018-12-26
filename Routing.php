b27ce20a0face893d27174e22bdba524ef4f5153
adrianwii <adrian.widlak@interia.pl>
2018-12-26 20:11:35 +0100

    AJAX requests

--- Routing.php
+++ Routing.php
@@ -1,8 +1,10 @@
 <?php
 
-require_once('controllers/DefaultController.php');
-require_once('controllers/UploadController.php');
-require_once('controllers/PlayerController.php');
+require_once 'controllers/DefaultController.php';
+require_once 'controllers/UploadController.php';
+require_once 'controllers/PlayerController.php';
+require_once 'controllers/AdminController.php';
+require_once 'controllers/AdminController.php';
 
 class Routing
 {
@@ -30,6 +32,18 @@ class Routing
             'player' => [
                 'controller' => 'PlayerController',
                 'action' => 'player'
+            ],
+            'admin' => [
+                'controller' => 'AdminController',
+                'action' => 'index'
+            ],
+            'admin_users' => [
+                'controller' => 'AdminController',
+                'action' => 'users'
+            ],
+            'admin_delete_user' => [
+                'controller' => 'AdminController',
+                'action' => 'userDelete'
             ]
         ];
     }
